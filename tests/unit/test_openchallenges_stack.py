4a59dd51f5108f4e38a0dc82006df05fa800a5eb
Khai Do <zaro0508@gmail.com>
2024-05-10 15:31:00 -0700

    Initial commit

---
+++ tests/unit/test_openchallenges_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from openchallenges.openchallenges_stack import OpenchallengesStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in openchallenges/openchallenges_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = OpenchallengesStack(app, "openchallenges")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
