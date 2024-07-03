73ea4a144d682b5656953d1bb2f0a85e196b4f0f
Khai Do <zaro0508@gmail.com>
2024-07-02 17:33:10 -0700

    Fix connection issues
    Some apps need to use TCP protocol while others need to use
    http protocol.

--- openchallenges/service_props.py
+++ openchallenges/service_props.py
@@ -8,11 +8,11 @@ class ServiceProps:
   """
   Shared stack properties
   """
-  def __init__(self, container_name:str, container_port:int, container_memory:int, container_repo_url:str,
+  def __init__(self, container_name:str, container_port:int, container_memory:int, container_location:str,
                container_env_vars:dict, web_path:str) -> None:
     self.container_name = container_name
     self.container_port = container_port
     self.container_memory = container_memory
-    self.container_repo_url = container_repo_url
+    self.container_location = container_location
     self.container_env_vars = container_env_vars
     self.web_path = web_path
