8314dc8dcbb84663c792756ecacec84636c971a0
Thomas Schaffter <thomas.schaffter@gmail.com>
2024-08-14 15:16:41 -0700

    add tools/setup.sh

---
+++ tools/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Safer bash scripts
+set -euxo pipefail
+
+npm install -g aws-cdk@2.151.0
+
+pip install --upgrade pip
+pip install -r requirements.txt -r requirements-dev.txt
+
+git config --global --add safe.directory "$PWD"
+pre-commit install-hooks
