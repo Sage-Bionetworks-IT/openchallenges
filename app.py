55506550260ee515fa55b67f4172160738026e0d
tschaffter <thomas.schaffter@gmail.com>
2024-11-27 19:23:01 +0000

    update data updated on

--- app.py
+++ app.py
@@ -303,9 +303,9 @@ oc_app_props = ServiceProps(
     f"ghcr.io/sage-bionetworks/openchallenges-app:{image_version}",
     {
         "API_DOCS_URL": f"https://{fully_qualified_domain_name}/api-docs",
-        "APP_VERSION": "1.1.1",
+        "APP_VERSION": image_version,
         "CSR_API_URL": f"https://{fully_qualified_domain_name}/api/v1",
-        "DATA_UPDATED_ON": "2024-11-13",
+        "DATA_UPDATED_ON": "2024-11-27",
         "ENVIRONMENT": "production",
         "GOOGLE_TAG_MANAGER_ID": "GTM-NBR5XD8C",
         "SSR_API_URL": "http://openchallenges-api-gateway:8082/api/v1",
