f186ed19bd68c572dface0d8b0c47aaea80c498d
tschaffter <thomas.schaffter@gmail.com>
2024-11-27 19:20:04 +0000

    Update to v1.1.1

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = cdk.App()
 # get the environment
 environment = utils.get_environment()
 stack_name_prefix = f"openchallenges-{environment}"
-image_version = "0.0.12"
+image_version = "1.1.1"
 
 # get VARS from cdk.json
 env_vars = app.node.try_get_context(environment)
@@ -303,7 +303,7 @@ oc_app_props = ServiceProps(
     f"ghcr.io/sage-bionetworks/openchallenges-app:{image_version}",
     {
         "API_DOCS_URL": f"https://{fully_qualified_domain_name}/api-docs",
-        "APP_VERSION": "1.0.12-beta",
+        "APP_VERSION": "1.1.1",
         "CSR_API_URL": f"https://{fully_qualified_domain_name}/api/v1",
         "DATA_UPDATED_ON": "2024-11-13",
         "ENVIRONMENT": "production",
