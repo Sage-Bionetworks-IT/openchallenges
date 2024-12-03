b7ca886279df8c907e514b7818b9256a770d451a
Khai Do <zaro0508@gmail.com>
2024-12-02 16:46:03 -0800

    [IT-3951] Fix guardduty container
    We enable guardduty security monitoring for ECS in every account.
    For that to work we need to give Fragate tasks access to do ECS
    stuff with the service-role/AmazonECSTaskExecutionRolePolicy[1].
    
    [1] https://docs.aws.amazon.com/guardduty/latest/ug/prereq-runtime-monitoring-ecs-support.html#before-enable-runtime-monitoring-ecs

--- openchallenges/service_stack.py
+++ openchallenges/service_stack.py
@@ -41,6 +41,9 @@ class ServiceStack(cdk.Stack):
             assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
             managed_policies=[
                 iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess"),
+                iam.ManagedPolicy.from_aws_managed_policy_name(
+                    "service-role/service-role/AmazonECSTaskExecutionRolePolicy"
+                ),
             ],
         )
         task_role.add_to_policy(
